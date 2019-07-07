d55f4b6443bb5c5b3f36bf3c2bf6fdb98599f823
S-Spencer <45274560+S-Spencer@users.noreply.github.com>
2019-07-07 14:11:20 +0100

    Day 14 part 2
    The code now checks the recipe list for sequential items that read as the number 890691 using new check function. As changes are only made to the end of the list, it is only necessary to check the last 6 digits (or 7 if a recipe of value > 10 was added).

--- day14.py
+++ day14.py
@@ -1,10 +1,24 @@
+import sys
+
+#Function to figure the new index for each elf after new recipies are created
 def step(elf,list):
-    move = list[elf] + elf +1
+    move = list[elf] + elf + 1
     if move >= len(list):
         move = move%len(list)
         return move
     else:
         return move
+
+#Function to check if the deisred number has been reached.
+def check(list,n):
+    if n == -6:
+        last_recipes = list[n:]
+    else:
+        last_recipes = list[n:n+6]
+    tail = "".join("{0}".format(i) for i in last_recipes)
+    tail = int(tail)
+    if tail == 890691:
+        sys.exit(print(len(list)+n))
 
 recipes = [3,7]
 #List to store the scores of each recipe
@@ -13,9 +27,12 @@ elfA = 0
 elfB = 1
 #Values to store the current recipe index of each elf
 
-while len(recipes) < 890701:
+while len(recipes) < 100000000:
     #The new recipie is found by summing the current recepie of each elf
     new_recipe = recipes[elfA] + recipes[elfB]
+    #The check function is used to see if the desired value has been reached yet.
+    check(recipes,-7)
+    check(recipes,-6)
     #The new recipe is added to the list, including a check to handle if it is double digits.
     if new_recipe > 9:
         recipes.append(1)
